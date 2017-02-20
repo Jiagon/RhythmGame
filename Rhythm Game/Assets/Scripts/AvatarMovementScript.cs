b3fbe18dcc239dcc163f34ca401b7d2219ac9a6b
James Taggart (RIT Student) <jxt1607@ad.rit.edu>
2017-02-20 18:57:11 -0500

    Add 2D assets and sprites. Add 3x3 disco fight/movement zone.

---
+++ Rhythm Game/Assets/Scripts/AvatarMovementScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarMovementScript : MonoBehaviour {
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown("up"))
+        {
+            //Debug.Log("Up Pressed");
+            if (transform.localPosition.y < 1)
+            {
+                transform.Translate(0, 1, 0);
+            }
+        }
+
+        if (Input.GetKeyDown("right"))
+        {
+            //Debug.Log("Right Pressed");
+            if(transform.localPosition.x< 1)
+            {
+                transform.Translate(1, 0, 0);
+            }
+        }
+
+        if (Input.GetKeyDown("down"))
+        {
+            //Debug.Log("Down Pressed");
+            if(transform.localPosition.y > -1)
+            {
+                transform.Translate(0, -1, 0);
+            }
+        }
+
+        if (Input.GetKeyDown("left"))
+        {
+            //Debug.Log("Left Pressed");
+            if(transform.localPosition.x > -1)
+            {
+                transform.Translate(-1, 0, 0);
+            }
+        }
+    }
+}
