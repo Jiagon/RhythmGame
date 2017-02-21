dff3a5b67e9947f8cb9ba3aad434d14ed6d12356
James Taggart (RIT Student) <jxt1607@ad.rit.edu>
2017-02-20 19:36:45 -0500

    Edits to ArenaTile script. Added unnecessary sprites. I'm just going to be switching their materials now instead of going through sprite sheets.

--- Rhythm Game/Assets/Scripts/ArenaTileScript.cs
+++ Rhythm Game/Assets/Scripts/ArenaTileScript.cs
@@ -6,24 +6,44 @@ public class ArenaTileScript : MonoBehaviour {
 
 
     [SerializeField]
-    bool isActive = false;
+    int dangerState = 0; //0 safe, 1 caution, 2 hurt
+    [SerializeField]
+    Material safe, danger, hurt;
+
+    [SerializeField]
+    float dangerTimer, hurtTimer;
+
+
+   
+
 
 	// Use this for initialization
 	void Start () {
-		
+       
+            
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update() {
+
+        switch (dangerState) {
+            case 0: GetComponentInChildren<Renderer>().material = safe;
+                break;
+            case 1: GetComponentInChildren<Renderer>().material = danger;
+                break;
+            case 2: GetComponentInChildren<Renderer>().material = hurt;
+                break;
+    }
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (isActive)
+        if (dangerState == 2)
         {
             Debug.Log("Colliding with active space at (" + this + ")");
+
         }
     }
 }
